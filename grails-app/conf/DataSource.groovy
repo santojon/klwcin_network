f1d5c6f3675f6d9da89ee68fea47bb11b6263642
Jonathan <jgs@cin.ufpe.br>
2014-08-07 17:17:23 -0300

    Release 2 improvements and database migration

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,4 +1,4 @@
-dataSource {
+/**dataSource {
     pooled = true
     jmxExport = true
     driverClassName = "org.h2.Driver"
@@ -53,4 +53,132 @@ environments {
             }
         }
     }
+}**/
+
+/**dataSource {
+    pooled = true
+    jmxExport = true
+    driverClassName = "org.mariadb.jdbc.Driver"
+        dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
+    username = "grailsdb"
+    password = "root"
+}
+hibernate {
+    cache.use_second_level_cache = true
+    cache.use_query_cache = false
+//    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
+    cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
+    singleSession = true // configure OSIV singleSession mode
+}
+
+// environment specific settings
+environments {
+    development {
+        dataSource {
+            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:mariadb://localhost:3306/klwcin_network_dev?autoreconnect=true"
+                        username = "grailsdb"
+                        password = "root"
+        }
+    }
+    test {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:mariadb://localhost:3306/klwcin_network_dev?autoreconnect=true"
+                        username = "grailsdb"
+                        password = "root"
+        }
+    }
+    production {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:mariadb://localhost:3306/klwcin_network_dev?autoreconnect=true"
+                        username = "grailsdb"
+                        password = "root"
+            properties {
+               // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
+               jmxEnabled = true
+               initialSize = 5
+               maxActive = 50
+               minIdle = 5
+               maxIdle = 25
+               maxWait = 10000
+               maxAge = 10 * 60000
+               timeBetweenEvictionRunsMillis = 5000
+               minEvictableIdleTimeMillis = 60000
+               validationQuery = "SELECT 1"
+               validationQueryTimeout = 3
+               validationInterval = 15000
+               testOnBorrow = true
+               testWhileIdle = true
+               testOnReturn = false
+               jdbcInterceptors = "ConnectionState"
+               defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
+            }
+        }
+    }
+}**/
+
+dataSource {
+	pooled = true
+	jmxExport = true
+	driverClassName = "com.mysql.jdbc.Driver"
+		dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
+	username = "root"
+	password = "root"
+}
+hibernate {
+	cache.use_second_level_cache = true
+	cache.use_query_cache = false
+	cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
+	//cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
+	singleSession = true // configure OSIV singleSession mode
+}
+
+// environment specific settings
+environments {
+	development {
+		dataSource {
+			dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+			url = "jdbc:mysql://localhost:3307/klwcin_network_dev?autoreconnect=true"
+						username = "root"
+						password = "root"
+		}
+	}
+	test {
+		dataSource {
+			dbCreate = "update"
+			url = "jdbc:mysql://localhost:3307/klwcin_network_dev?autoreconnect=true"
+						username = "root"
+						password = "root"
+		}
+	}
+	production {
+		dataSource {
+			dbCreate = "update"
+			url = "jdbc:mysql://localhost:3307/klwcin_network_dev?autoreconnect=true"
+						username = "root"
+						password = "root"
+			properties {
+			   // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
+			   jmxEnabled = true
+			   initialSize = 5
+			   maxActive = 50
+			   minIdle = 5
+			   maxIdle = 25
+			   maxWait = 10000
+			   maxAge = 10 * 60000
+			   timeBetweenEvictionRunsMillis = 5000
+			   minEvictableIdleTimeMillis = 60000
+			   validationQuery = "SELECT 1"
+			   validationQueryTimeout = 3
+			   validationInterval = 15000
+			   testOnBorrow = true
+			   testWhileIdle = true
+			   testOnReturn = false
+			   jdbcInterceptors = "ConnectionState"
+			   defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
+			}
+		}
+	}
 }
--- grails-app/views/layouts/main.gsp
+++ grails-app/views/layouts/main.gsp
@@ -18,13 +18,13 @@
 	</head>
 	<body>
 		<div id="grailsLogo" role="banner">
-			<a href="http://www.cin.ufpe.br/~jgs/klwcin.html"><asset:image src="Icon.png" alt="KLWCIn logo"/></a>
+			<a href="http://www.cin.ufpe.br/~jgs/klwcin.html"><asset:image src="logo_full.png" alt="KLWCIn logo"/>KLWCIn Network</a>
 			<div class="nav" role="navigation">
 				<form action="${createLink(uri: '/j_spring_security_logout')}">
 					<button formaction="${createLink(uri: '/user')}">Manage Users</button>
 					<button formaction="${createLink(uri: '/meeting')}">Manage Meetings</button>
-					<button formaction="${createLink(uri: '/login/auth')}" style="float: right;">Login</button>
 					<button formaction="${createLink(uri: '/j_spring_security_logout')}" style="float: right;">Logout</button>
+					<button formaction="${createLink(uri: '/login/auth')}" style="float: right;">Login</button>
 				</form>
 			</div>
 		</div>
